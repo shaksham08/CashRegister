85783bece68798b2ece0af2ff137105cd0cdd313
shaksham08 <shakshamsinha@outlook.com>
2021-09-24 22:57:24 +0530

    fix - fixed the change count

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideError();
   if (billAmount.value > 0) {
     if (parseInt(cashGiven.value) >= parseInt(billAmount.value)) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
-      calculateChange(amountToBeReturned);
+      let amountToBeReturned = cashGiven.value - billAmount.value;
+      for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(
+          amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+      }
     } else {
       showError("Hey, Dont you have money?");
     }
@@ -39,10 +45,4 @@ function showError(msg) {
   message.innerText = msg;
 }
 
-function calculateChange(amountToBeReturned) {
-  for (let i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
-    noOfNotes[i].innerText = numberOfNotes;
-  }
-}
+function calculateChange(amountToBeReturned) {}
